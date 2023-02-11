433c071b2fa30bba35c306b8d55551897c7fbbce
Spyglass Software <tyler@spyglass.software>
2023-02-11 11:01:26 -0500

    apply: get ready for sql commands

--- src/commands/apply.ts
+++ src/commands/apply.ts
@@ -1,8 +1,7 @@
-/* eslint-disable max-depth */
 import {Args, Command, Flags} from '@oclif/core'
 import {readFile} from 'node:fs/promises'
 import color from '@oclif/color';
-import {parse, stringify} from 'yaml'
+import {parse} from 'yaml'
 import {apiCall} from '../lib/api'
 import {getConfig} from '../lib/config'
 
@@ -11,6 +10,7 @@ export default class Apply extends Command {
 
   static flags = {
     'dry-run': Flags.boolean({description: 'Dry run', default: false}),
+    confirm: Flags.boolean({description: 'Skip the interactive prompt (used in CI)', default: false}),
   }
 
   static args = {
@@ -27,65 +27,38 @@ export default class Apply extends Command {
 
     const payload = {
       action: 'apply',
-      dryRun: flags['dry-run'],
+      dryRun: true, // first run is always dry, so we can show user what will happen
       files: {
         [args.filename]: contents,
       },
     }
 
-    const now = Math.floor((Date.now() / 1000)) // we should return timestamp from API instead
     const res = await apiCall(cfg, payload)
     if (res.data.error) {
       this.log(`Encountered an error: ${res.data.error}, code: ${res.data.code}`)
       return
     }
 
-    // show raw differences
-    // show sql differences
-    // if dry run, show both
-    // if not dry run, ask confirmation to apply
-    // (if --confirm, skip confirmation)
+    // Print SQL differences.
+    this.log(res.data.sqlToExecute)
 
-    const yamlDiffs: YamlDiff = res.data.yamlDiffs
-    for (const [filename, yamlDiff] of Object.entries(yamlDiffs)) {
-      this.log(color.bold(`diff --spyglass a/snowflake:${now} b/${filename}`))
-      this.log(color.bold(`--- a/snowflake:${now}`))
-      this.log(color.bold(`--- b/${filename}`))
-
-      for (const [roleName, role] of Object.entries(contents)) {
-        let hasDiffs = false
-
-        for (const [grantName, grants] of Object.entries(role)) {
-          for (const [i, grant] of grants.entries()) {
-            if (yamlDiff.added?.[roleName]?.[grantName]?.includes(grant)) {
-              grants[i] = color.green('+  - ' + grant)
-              hasDiffs = true
-            } else {
-              grants[i] = '   - ' + grant
-            }
-          }
-
-          for (const deletedGrant of (yamlDiff.deleted?.[roleName]?.[grantName] || [])) {
-            grants.push(color.red('-  - ' + deletedGrant))
-            hasDiffs = true
-          }
-
-          if (hasDiffs) {
-            this.log(color.cyan(`@@ role:${roleName} @@`))
-
-            // HACK: stringify yaml ourselves, it only goes one level deep, only displays arrays
-            for (const [grantName, grants] of Object.entries(role)) {
-              if (grants?.length > 0) {
-                this.log(grantName + ':')
-                for (const grant of grants) {
-                  this.log(grant)
-                }
-              }
-            }
-          }
-        }
-      }
+    // We can exit if this is a dry run.
+    if (flags['dry-run']) {
+      return
     }
+
+    // If confirm isn't provided, get interactive confirmation from user.
+    if (!flags.confirm) {
+      return
+    }
+
+    // Apply configuration to production.
+    payload.dryRun = false
+    // const res = await apiCall(cfg, payload)
+    // if (res.data.error) {
+    //   this.log(`Encountered an error: ${res.data.error}, code: ${res.data.code}`)
+    //   return
+    // }
   }
 }
 
