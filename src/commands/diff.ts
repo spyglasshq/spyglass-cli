5a8a768819f44e032b84eb31fadf66eed5181dac
Spyglass Software <tyler@spyglass.software>
2023-02-12 22:27:47 -0500

    fix up diff

--- src/commands/diff.ts
+++ src/commands/diff.ts
@@ -1,15 +1,19 @@
 /* eslint-disable max-depth */
 import {Args, Command, ux} from '@oclif/core'
-import color from '@oclif/color';
+import color from '@oclif/color'
 import {apiCall} from '../lib/api'
 import {getConfig} from '../lib/config'
-import {readYamlFile, YamlDiff} from '../lib/yaml';
+import {readYamlFile, YamlDiff} from '../lib/yaml'
+import {stringify} from 'yaml'
+
+const replacer = (key: string, value: unknown) => value === undefined ? null : value
 
 export default class Diff extends Command {
   static description = 'Convert Spyglass configuration to native database commands and execute them.'
 
   static args = {
-    filename: Args.string({description: 'File to use for diffing with Snowflake live configuration.', required: true}),
+    'current-file': Args.string({description: 'Current account configuration yaml.', required: true}),
+    'proposed-file': Args.string({description: 'Proposed changes to account configuration yaml.', required: true}),
   }
 
   async run(): Promise<void> {
@@ -17,17 +21,19 @@ export default class Diff extends Command {
 
     const cfg = await getConfig(this.config.configDir)
 
-    const contents = await readYamlFile(args.filename)
+    const currentFile = args['current-file']
+    const proposedFile = args['proposed-file']
+
+    const current = await readYamlFile(currentFile)
+    const proposed = await readYamlFile(proposedFile)
 
     const payload = {
       action: 'diff',
-      files: {
-        [args.filename]: contents,
-      },
+      currentFiles: [current],
+      proposedFiles: [proposed],
     }
 
-    const now = Math.floor((Date.now() / 1000)) // we should return timestamp from API instead
-    ux.action.start('Fetching current Snowflake configuration')
+    ux.action.start('Diffing configuration files')
     const res = await apiCall(cfg, payload)
     ux.action.stop()
 
@@ -36,44 +42,37 @@ export default class Diff extends Command {
       return
     }
 
-    const yamlDiffs: YamlDiff = res.data.yamlDiffs
-    for (const [filename, yamlDiff] of Object.entries(yamlDiffs)) {
-      this.log(color.bold(`diff --spyglass a/snowflake:${now} b/${filename}`))
-      this.log(color.bold(`--- a/snowflake:${now}`))
-      this.log(color.bold(`--- b/${filename}`))
+    const yamlDiffs: YamlDiff[] = res.data.yamlDiffs
+    // this.log(stringify(yamlDiffs))
+    for (const yamlDiff of yamlDiffs) {
+      this.log(color.bold(`diff --spyglass a/${currentFile} b/${proposedFile}`))
+      this.log(color.bold(`--- a/${currentFile}`))
+      this.log(color.bold(`--- b/${proposedFile}`))
 
-      for (const [roleName, role] of Object.entries(contents.roles)) {
-        let hasDiffs = false
-
-        for (const [grantName, grants] of Object.entries(role)) {
-          for (const [i, grant] of grants.entries()) {
-            if (yamlDiff.added?.[roleName]?.[grantName]?.includes(grant)) {
-              grants[i] = color.green('+  - ' + grant)
-              hasDiffs = true
-            } else {
-              grants[i] = '   - ' + grant
-            }
+      for (const [roleName, role] of Object.entries(yamlDiff.added)) {
+        this.log(color.cyan(`@@ role:${roleName} @@`))
+        const yamlRoleLines = stringify(role, replacer).split('\n')
+        const printRole = yamlRoleLines.map((line, i) => {
+          if (i === yamlRoleLines.length - 1) {
+            return '  ' + line
           }
 
-          for (const deletedGrant of (yamlDiff.deleted?.[roleName]?.[grantName] || [])) {
-            grants.push(color.red('-  - ' + deletedGrant))
-            hasDiffs = true
+          return color.green('+ ' + line)
+        }).join('\n')
+        this.log(printRole)
+      }
+
+      for (const [roleName, role] of Object.entries(yamlDiff.deleted)) {
+        this.log(color.cyan(`@@ role:${roleName} @@`))
+        const yamlRoleLines = stringify(role, replacer).split('\n')
+        const printRole = yamlRoleLines.map((line, i) => {
+          if (i === yamlRoleLines.length - 1) {
+            return '  ' + line
           }
 
-          if (hasDiffs) {
-            this.log(color.cyan(`@@ role:${roleName} @@`))
-
-            // HACK: stringify yaml ourselves, it only goes one level deep, only displays arrays
-            for (const [grantName, grants] of Object.entries(role)) {
-              if (grants?.length > 0) {
-                this.log(grantName + ':')
-                for (const grant of grants) {
-                  this.log(grant)
-                }
-              }
-            }
-          }
-        }
+          return color.red('- ' + line)
+        }).join('\n')
+        this.log(printRole)
       }
     }
   }
