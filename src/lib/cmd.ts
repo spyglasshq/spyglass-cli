45033565b9e95a2b68526ebc3037add287507097
Spyglass Software <tyler@spyglass.software>
2023-03-12 15:17:45 -0400

    fail open on logging errors #9

--- src/lib/cmd.ts
+++ src/lib/cmd.ts
@@ -5,10 +5,15 @@ import {getLogger, getNoopLogger, LOG_COMMAND_ERROR, LOG_COMMAND_SUCCESS} from '
 
 export interface LoggableError {
   message: string;
+  stack: string;
 }
 
 export abstract class BaseCommand extends Command {
   logger: winston.Logger
+
+  // exiting tracks when we have started to shutdown the command
+  // if we're exiting, we skip things like logging so we don't get 'write after end' errors
+  exiting = false
 
   static baseFlags = {
     dir: Flags.string({
@@ -32,30 +37,53 @@ export abstract class BaseCommand extends Command {
   async logSuccess(): Promise<void> {
     return new Promise(resolve => {
       this.logger.on('finish', resolve)
+
+      this.logger.on('error', (error) => {
+        if (!this.exiting) {
+          this.log(error.stack)
+          this.log('Logging error encountered, exiting cleanly.')
+        }
+        resolve()
+      })
+
       this.logger.info(LOG_COMMAND_SUCCESS, {command: this.id, args: this.argv})
+
       this.logger.end()
     })
   }
 
   async logSuccessAndExit(): Promise<void> {
     await this.logSuccess()
+    this.exiting = true
     this.exit()
   }
 
-  async logError({message}: LoggableError): Promise<void> {
+  async logError({message, stack}: LoggableError): Promise<void> {
     return new Promise(resolve => {
       this.logger.on('finish', resolve)
+
+      this.logger.on('error', (error) => {
+        if (!this.exiting) {
+          this.log(error.stack)
+          this.log('Logging error encountered, exiting cleanly.')
+        }
+        resolve()
+      })
+
       this.logger.error(LOG_COMMAND_ERROR, {
         command: this.id,
         args: this.argv,
         error: message,
+        stack,
       })
+
       this.logger.end()
     })
   }
 
   async logErrorAndExit(error: LoggableError): Promise<void> {
     await this.logError(error)
+    this.exiting = true
     this.exit(1)
   }
 
