07f8945a73b011a783ac6b0f50c2f2f8ec6ce436
Spyglass Software <tyler@spyglass.software>
2023-03-17 08:40:54 -0400

    disable login for now

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,6 +1,4 @@
-import {Command, ux} from '@oclif/core'
-import {apiCall} from '../lib/api'
-import {createOrUpdateConfig} from '../lib/config'
+import {Command} from '@oclif/core'
 
 export default class Login extends Command {
   static hidden = true
@@ -8,31 +6,32 @@ export default class Login extends Command {
   static description = 'Log in to your Spyglass account.'
 
   async run(): Promise<void> {
-    await this.init()
+    this.log('Not enabled')
+    // await this.init()
 
-    const teamId = await ux.prompt('What is your team id? (e.g. "example.com")')
+    // const teamId = await ux.prompt('What is your team id? (e.g. "example.com")')
 
-    this.log('First, go to https://demo.spyglass.software/connect and find your personal access token.')
-    const personalAccessToken = await ux.prompt('Next, paste the token', {type: 'mask'})
+    // this.log('First, go to https://demo.spyglass.software/connect and find your personal access token.')
+    // const personalAccessToken = await ux.prompt('Next, paste the token', {type: 'mask'})
 
-    const cfg = {
-      teamId,
-      personalAccessToken,
-      dev: Boolean(process.env.SPYGLASS_DEV),
-    }
-    const payload = {
-      action: 'auth',
-    }
-    const res = await apiCall(cfg, payload)
-    if (res.data.error) {
-      this.log(`Encountered an error: ${res.data.error}, code: ${res.data.code}`)
-      return
-    }
+    // const cfg = {
+    //   teamId,
+    //   personalAccessToken,
+    //   dev: Boolean(process.env.SPYGLASS_DEV),
+    // }
+    // const payload = {
+    //   action: 'auth',
+    // }
+    // const res = await apiCall(cfg, payload)
+    // if (res.data.error) {
+    //   this.log(`Encountered an error: ${res.data.error}, code: ${res.data.code}`)
+    //   return
+    // }
 
-    this.log('Verified personal access token is valid!')
+    // this.log('Verified personal access token is valid!')
 
-    await createOrUpdateConfig(this.config.configDir, cfg)
+    // await createOrUpdateConfig(this.config.configDir, cfg)
 
-    this.log('Successfully updated your access token!')
+    // this.log('Successfully updated your access token!')
   }
 }
