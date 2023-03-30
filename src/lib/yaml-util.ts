bb308c348cf5bdb5912714d8a534aeaf2ef547b8
Spyglass Software <tyler@spyglass.software>
2023-03-30 15:58:54 -0400

    verify/sr1001: catch usage database issue for all objects
    fixes #41

---
+++ src/lib/yaml-util.ts
@@ -0,0 +1,27 @@
+import {YamlRole, YamlRoles} from './yaml'
+
+interface ObjectIdIter {
+  roleName: string;
+  roleInfo: YamlRole;
+  privilege: string;
+  objectType: string;
+  objectId: string;
+}
+
+export function forEachObjectInRoleGrants(roleGrants: YamlRoles, fn: (_: ObjectIdIter) => void): void {
+  for (const [roleName, roleInfo] of Object.entries(roleGrants)) {
+    for (const [privilege, objectLists] of Object.entries(roleInfo)) {
+      for (const [objectType, objectIds] of Object.entries(objectLists)) {
+        for (const objectId of objectIds) {
+          fn({
+            roleName,
+            roleInfo,
+            privilege,
+            objectType,
+            objectId,
+          })
+        }
+      }
+    }
+  }
+}
