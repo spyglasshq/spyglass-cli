54f1ad87cc3406350891548b19580381871f2a36
Spyglass Software <tyler@spyglass.software>
2023-03-31 09:18:52 -0400

    add utility to divide files and write them out

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,9 @@
 import * as yaml from './yaml'
+import * as yamlFiles from './yaml-files'
 import * as snowflake from './snowflake'
 
 export {
   yaml,
   snowflake,
+  yamlFiles,
 }
--- src/lib/yaml-files.ts
+++ src/lib/yaml-files.ts
@@ -1,4 +1,4 @@
-import {readFile, writeFile} from 'node:fs/promises'
+import {mkdir, readFile, writeFile} from 'node:fs/promises'
 import {parse, stringify} from 'yaml'
 import {exists} from 'fs-extra'
 import path = require('node:path')
@@ -32,4 +32,66 @@ export async function writeYamlForAccountId(accountId: string, yaml: Yaml, dir =
 
 export async function writeYamlFile(filename: string, yaml: Yaml): Promise<void> {
   await writeFile(filename, stringify(yaml, {sortMapEntries: true}))
+}
+
+export interface DividedYaml {
+  [path: string]: any // Should be Yaml after we make more fields optional
+}
+
+/**
+ * Divides the yaml into groups.
+ *
+ * @param yaml Yaml to split.
+ *
+ * @returns void
+ * @experimental
+ */
+export async function divideYaml(yaml: Yaml): Promise<DividedYaml> {
+  const res: DividedYaml = {}
+
+  for (const [roleName, roleDef] of Object.entries(yaml.roles)) {
+    res[`roles/${roleName}`] = {
+      roles: {
+        [roleName]: roleDef,
+      },
+      roleGrants: {
+        [roleName]: yaml.roleGrants[roleName],
+      },
+    }
+  }
+
+  for (const [username, userInfo] of Object.entries(yaml.userGrants)) {
+    res[`users/${username}`] = {
+      userGrants: {
+        [username]: userInfo,
+      },
+    }
+  }
+
+  res.spyglass = {
+    spyglass: yaml.spyglass,
+  }
+
+  return res
+}
+
+/**
+ * Writes the yaml to files, creating directories along the way.
+ *
+ * @param dividedYaml Divided yaml to write to files.
+ *
+ * @returns void
+ * @experimental
+ */
+export async function writeDividedYamlToFiles(dividedYaml: DividedYaml): Promise<void> {
+  for (const [name, yaml] of Object.entries(dividedYaml)) {
+    const filepath = path.join('./spyglass', `${name}.yaml`)
+    const dir = path.dirname(filepath)
+
+    // eslint-disable-next-line no-await-in-loop
+    await mkdir(dir, {recursive: true})
+
+    // eslint-disable-next-line no-await-in-loop
+    await writeFile(filepath, stringify(yaml, {sortMapEntries: true}))
+  }
 }
