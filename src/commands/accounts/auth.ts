5cac68db62df118d34aceae439efdb1a578b146e
Spyglass Software <tyler@spyglass.software>
2023-04-02 10:29:48 -0400

    accounts/auth: support multiple accounts
    also normalize account ids
    
    fixes #80

--- src/commands/accounts/auth.ts
+++ src/commands/accounts/auth.ts
@@ -18,15 +18,17 @@ export default class Auth extends BaseCommand {
 
     const {args, flags} = await this.parse(Auth)
 
+    const accountId = args.accountId.toLowerCase()
+
     const config = await getSnowflakeConfig()
 
-    const existingConn = Object.values(config?.connections ?? {})?.find(conn => conn?.accountname === args.accountId)
+    const existingConn = Object.values(config?.connections ?? {})?.find(conn => conn?.accountname === accountId)
     if (existingConn) {
-      this.log(`There is already a connection configured in ~/.snowsql/config for account "${args.accountId}"`)
+      this.log(`There is already a connection configured in ~/.snowsql/config for account "${accountId}"`)
 
       if (flags.check) {
         ux.action.start('Checking connection')
-        await checkConnection(args.accountId)
+        await checkConnection(accountId)
         ux.action.stop()
         this.log('Success!')
       }
@@ -35,7 +37,7 @@ export default class Auth extends BaseCommand {
     }
 
     if (config) {
-      this.log(`We found config at ~/.snowsql/config, but nothing for account "${args.accountId}". Let's add that account!`)
+      this.log(`We found config at ~/.snowsql/config, but nothing for account "${accountId}". Let's add that account!`)
     } else {
       this.log('Welcome to Spyglass! Let\'s set up a ~/.snowsql/config file.')
       this.log('(See https://docs.snowflake.com/en/user-guide/snowsql-config for more info)')
@@ -47,17 +49,25 @@ export default class Auth extends BaseCommand {
     const warehouse = ''
     const role = ''
 
-    const newConfig: Config = {
-      connections: {
-        [args.accountId]: {accountname: args.accountId, username, password, warehousename: warehouse, rolename: role, authenticator: AUTHENTICATOR_PASSWORD},
-      },
+    if (config) {
+      const connections = config.connections ?? {}
+      connections[accountId] = {accountname: accountId, username, password, warehousename: warehouse, rolename: role, authenticator: AUTHENTICATOR_PASSWORD}
+      config.connections = connections
+      await saveConfig(config)
+    } else {
+      const newConfig: Config = {
+        connections: {
+          [accountId]: {accountname: accountId, username, password, warehousename: warehouse, rolename: role, authenticator: AUTHENTICATOR_PASSWORD},
+        },
+      }
+      await saveConfig(newConfig)
     }
 
-    await saveConfig(newConfig)
+
 
     if (flags.check) {
       ux.action.start('Checking connection')
-      await checkConnection(args.accountId)
+      await checkConnection(accountId)
       ux.action.stop()
       this.log('Success!')
     }
