4ce8472a5709fba395421b99e06fa0301a6e8078
Spyglass Software <tyler@spyglass.software>
2023-04-14 08:32:19 -0400

    release action: tee to changelog

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,7 +13,7 @@ latest_version=$(jq -r .version package.json)
 
 # Add release notes to changelog
 head -n1 CHANGELOG.md >CHANGELOG.md.tmp
-npx lerna-changelog --from="v$previous_version" --to="v$latest_version" >>CHANGELOG.md.tmp
+npx lerna-changelog --from="v$previous_version" --to="v$latest_version" | tee -a CHANGELOG.md.tmp
 tail -n +2 CHANGELOG.md >>CHANGELOG.md.tmp
 
 mv CHANGELOG.md.tmp CHANGELOG.md
