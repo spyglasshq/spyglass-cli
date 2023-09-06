cf74a75f7592b4c55286414339d70005a670a378
Spyglass Software <tyler@spyglass.software>
2023-09-06 11:19:40 -0400

    unit tests

--- src/lib/ratelimit.ts
+++ src/lib/ratelimit.ts
@@ -1,3 +1,6 @@
+
+export type SetInterval = (callback: (args: void) => void, ms?: number) => NodeJS.Timer
+
 export class RateLimiter {
   private rps: number
   private interval: NodeJS.Timer
@@ -6,16 +9,21 @@ export class RateLimiter {
 
   private resolvers: ((value: void | PromiseLike<void>) => void)[]
 
-  constructor(rps: number) {
+  constructor(rps: number, _setInterval: SetInterval = setInterval) {
     this.closed = false
     this.rps = rps
     this.resolvers = []
 
     const sliceFactor = 5 // Arbitrarily update the count every 5th of a second, slightly more smooth than every 1 second.
-    this.interval = setInterval(() => {
+    this.interval = _setInterval(() => {
       if (this.resolvers.length === 0) {
         return
       }
+
+      // NOTE: Technically, this implementation works best when all the requests are queued up in a large batch,
+      // and isn't quite correct if requests stream in over time. This is because every 5th of a second, we are pulling
+      // 2 requests off of the request queue. So if there aren't any requests in this time chunk, we won't try to "catch up"
+      // on the next tick. This is acceptable given our usage.
 
       const numCalls = Math.floor(this.rps / sliceFactor)
       for (let i = 0; i < numCalls; i++) {
@@ -41,34 +49,52 @@ export class RateLimiter {
     })
   }
 
-  async close(): Promise<void> {
+  close(): void {
     clearInterval(this.interval)
   }
 }
 
 export class WaitGroup {
   private count: number
+  private interval?: NodeJS.Timer
+  private closed: boolean
 
   constructor() {
     this.count = 0
+    this.closed = false
   }
 
-  add(): void {
-    this.count += 1
+  add(n = 1): void {
+    if (this.closed) {
+      throw new Error('wait group is closed')
+    }
+
+    const newCount = this.count + n
+
+    if (newCount < 0) {
+      throw new Error('negative wait group counter')
+    }
+
+    this.count = newCount
   }
 
   done(): void {
-    this.count -= 1
+    this.add(-1)
   }
 
   async wait(): Promise<void> {
     return new Promise(resolve => {
-      const interval = setInterval(() => {
+      this.interval = setInterval(() => {
         if (this.count === 0) {
-          clearInterval(interval)
+          this.close()
           resolve()
         }
-      }, 1000)
+      }, 100)
     })
+  }
+
+  close(): void {
+    this.closed = true
+    clearInterval(this.interval)
   }
 }
